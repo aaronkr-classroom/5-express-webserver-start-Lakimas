b2bbd840a6211ab7443304dda7e1e94d2405ad24
Kim MinChan <163220362+Lakimas@users.noreply.github.com>
2024-04-17 01:44:04 +0000

    10,11끝!

--- lesson-10/main.js
+++ lesson-10/main.js
@@ -2,3 +2,21 @@
 "use strict";
 
 // @TODO: 웹 서버를 만들고 실행한다.
+const port = 3000,
+express = require('express'),
+layout = require('express-ejs-layouts'),
+homeController = require('./controllers/homeController'),
+app = express();
+
+app.set("port",process.env.PORT || port);
+app.set("view engine","ejs");
+
+app.use(layout); //
+
+app.get('./name/:myName', homeController.respondWithName);
+// app.get(); = GET method
+// app.post(); = POST method
+
+app.listen(app.get("port"),() =>{
+    console.log(`Sever at http://localhost:${app.get("port")}`)
+});
--- lesson-11/main.js
+++ lesson-11/main.js
@@ -2,3 +2,27 @@
 "use strict";
 
 // @TODO: 웹 서버를 만들고 실행한다.
+const port = 3001,
+express = require('express'),
+layout = require('express-ejs-layouts'),
+homeController = require('./controllers/homeController'),
+errorController = require('./controllers/errorController'),
+app = express();
+
+app.set("port",process.env.PORT || port);
+app.set("view engine","ejs");
+
+app.use(layout); //
+app.use(express.static("public"));//정적파일 디렉토리
+
+app.get('/name/:myName', homeController.respondWithName);
+// app.get(); = GET method
+// app.post(); = POST method
+
+app.listen(app.get("port"),() =>{
+    console.log(`Sever at http://localhost:${app.get("port")}`)
+});
+
+app.use(errorController.logErrors);
+app.use(errorController.resNotFound);
+app.use(errorController.resInternalError);
